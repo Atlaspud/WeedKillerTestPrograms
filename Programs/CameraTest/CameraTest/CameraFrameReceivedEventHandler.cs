a1ba0b16e3157e5412caf07862131f4583db1e68
AlexOlsen <alex.olsen@my.jcu.edu.au>
2014-11-27 17:00:15 +1000

    First working version of Camera class in CameraTest application. Demonstrates use of Camera and LightSensor classes.

---
+++ Programs/CameraTest/CameraTest/CameraFrameReceivedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace CameraTest
+{
+    public delegate void CameraFrameReceivedEventHandler(object sender, CameraFrameReceivedEventArgs e);
+
+    public class CameraFrameReceivedEventArgs : EventArgs
+    {
+        public Image<Bgr, Byte> image { get; set; }
+        public DateTime time { get; set; }
+        public uint serial { get; set; }
+        public string debug { get; set; }
+    }
+}
