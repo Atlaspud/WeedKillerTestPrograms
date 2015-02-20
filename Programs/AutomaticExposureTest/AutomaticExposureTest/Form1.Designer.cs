fe998045a7cb0cb5a3f0771753b062adfaf991fb
unknown <jc210083@C02MC07VF8J7-7.ad.jcu.edu.au>
2015-02-20 14:53:56 +1000

    Added Automatic Exposure Test program. Added waitForImage() function to Camera class.

---
+++ Programs/AutomaticExposureTest/AutomaticExposureTest/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace AutomaticExposureTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.runButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(984, 562);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(249, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(732, 556);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(240, 556);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.runButton, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(234, 272);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // runBtn
+            // 
+            this.runButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.runButton.Location = new System.Drawing.Point(3, 3);
+            this.runButton.Name = "runBtn";
+            this.runButton.Size = new System.Drawing.Size(228, 266);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Start";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(3, 281);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(234, 272);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 562);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Programs/AutomaticExposureTest/AutomaticExposureTest/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace AutomaticExposureTest
+{
+    public partial class Form1 : Form
+    {
+
+        bool stop;
+        Camera camera;
+        List<Image<Bgr, Byte>> images;
+
+        public static readonly uint[] SerialNumbers = new uint[8]
+        {
+            13421033,
+            13421041,
+            13421043,
+            13421046,
+            13421051,
+            13421053,
+            13421056,
+            13421057
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialiseSystem();
+        }
+
+        private Boolean initialiseVision()
+        {
+            int cameraCount = Camera.GetNumberOfCameras();
+            if (cameraCount != 0)
+            {
+                AppendLine(String.Format("Cameras Found: {0}", cameraCount));
+                camera = new Camera(SerialNumbers[0]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean initialiseSystem()
+        {
+            if (!initialiseVision())
+            {
+                MessageBox.Show("Failed to initialise cameras. Check cameras and try again.", "Error",MessageBoxButtons.OK);
+                runButton.Text = "Initialise";
+                return false;
+            }
+            return true;
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            switch (runButton.Text)
+            {
+                case "Start":
+                    runButton.Text = "Stop";
+                    startSystem();
+                    break;
+
+                case "Stop":
+                    runButton.Text = "Start";
+                    stopSystem();
+                    break;
+
+                case "Initialise":
+                    if (initialiseSystem())
+                    {
+                        runButton.Text = "Start";
+                    }
+                    break;
+            }
+        }
+
+        public void startSystem()
+        {
+            stop = false;
+            camera.start();
+            Thread systemThread = new Thread(systemLoop);
+            systemThread.Start();
+        }
+
+        public void stopSystem()
+        {
+            stop = true;
+            camera.stop();
+        }
+
+        private void systemLoop()
+        {
+            while (!stop)
+            {
+                images.Add(camera.waitForImage());
+                pictureBox.Image = images[images.Count - 1].Bitmap;
+                AppendLine("" + images.Count);
+            }
+        }
+
+        delegate void AppendLineCallback(string text);
+
+        public void AppendLine(string text)
+        {
+
+            if (this.textBox.InvokeRequired)
+            {
+                AppendLineCallback d = new AppendLineCallback(AppendLine);
+                this.BeginInvoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox.AppendText(text + Environment.NewLine);
+            }
+        }
+    }
+}
